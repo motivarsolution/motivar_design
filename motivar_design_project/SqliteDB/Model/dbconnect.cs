306949651870f336a40fd634226dc317cc534761
TΛП WUTTISAKDANON <hadson_sm@hotmail.com>
2017-05-17 23:25:36 +0700

    Add SqliteDB Project

---
+++ motivar_design_project/SqliteDB/Model/dbconnect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqliteDB.Model
+{
+    public static class dbconnect
+    {
+        public static void dbCreate()
+        {
+            SQLiteConnection.CreateFile("MyDatabaseSQLITE.sqlite");
+        }
+    }
+}
---
+++ motivar_design_project/SqliteDB/dbForm.cs
@@ -0,0 +1,22 @@
+﻿using SqliteDB.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqliteDB
+{
+    public partial class dbForm : Form
+    {
+        public dbForm()
+        {
+            InitializeComponent();
+            dbconnect.dbCreate();
+        }
+    }
+}
