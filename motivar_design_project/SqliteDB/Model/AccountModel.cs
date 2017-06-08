3f319c66a9bc6967e8e47d34f23425847fed3ba8
TΛП WUTTISAKDANON <hadson_sm@hotmail.com>
2017-06-08 14:42:19 +0700

    -Sqlite Connecttion -Sqlite To List -List To DataSet

--- motivar_design_project/SqliteDB/Model/AccountModel.cs
+++ motivar_design_project/SqliteDB/Model/AccountModel.cs
@@ -9,23 +9,29 @@ namespace SqliteDB.Model
     public class AccountModel
     {
         //----------------Singleton-----------------
-        private static AccountModel instance;
+        //private static AccountModel instance;
 
-        private AccountModel() { }
+        //private AccountModel() { }
 
-        public static AccountModel Instance
-        {
-            get
-            {
-                if(instance == null)
-                {
-                    instance = new AccountModel();
-                }
-                return instance;
-            }
-        }
+        //public static AccountModel Instance
+        //{
+        //    get
+        //    {
+        //        if(instance == null)
+        //        {
+        //            instance = new AccountModel();
+        //        }
+        //        return instance;
+        //    }
+        //}
         //---------------------------------------
         //TEST GITHUB
         //RESEND
+
+        public string AccountID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+        public string Roles { get; set; }
     }
 }
--- motivar_design_project/SqliteDB/dbForm.cs
+++ motivar_design_project/SqliteDB/dbForm.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Data.SQLite;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,9 +15,11 @@ namespace SqliteDB
 {
     public partial class dbForm : Form
     {
+        private List<AccountModel> AcList = new List<AccountModel>();
         private SQLiteConnection sql_con;
         private SQLiteCommand sql_cmd;
         private SQLiteDataAdapter DB;
+        private SQLiteDataReader RD;
         private DataSet DS = new DataSet("SqliteDataSet");
         private DataTable DT = new DataTable("Account");
 
@@ -30,17 +33,112 @@ namespace SqliteDB
             DB = new SQLiteDataAdapter(CommandText, sql_con);
             DS.Reset();
             DB.Fill(DS);
-            DT.TableName = "Account";
             DT = DS.Tables[0];
             Grid.DataSource = DT;
             sql_con.Close();
             
         }
 
+        private void dbReadToList()
+        {
+            SetConnection();
+            sql_con.Open();
+            sql_cmd = sql_con.CreateCommand();
+            string CommandText = "select * from Account";
+
+            using (SQLiteCommand cmd = new SQLiteCommand(CommandText, sql_con))
+            {
+                using (SQLiteDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        AcList.Add(new AccountModel { AccountID = rdr["AccountID"].ToString()
+                                                      ,Username = rdr["Username"].ToString()
+                                                      ,Password = rdr["Password"].ToString()
+                                                      ,DisplayName = rdr["DisplayName"].ToString()
+                                                      ,Roles = rdr["Roles"].ToString()});
+                    }
+                }
+            }
+            
+            sql_con.Close();
+
+        }
+
+        private void ListToDataSet()
+        {
+            DataTable dt = new DataTable("AccountTable");
+            Type t = typeof(AccountModel);
+            PropertyInfo[] pia = t.GetProperties();
+
+            //Inspect the properties and create the columns in the DataTable
+            foreach (PropertyInfo pi in pia)
+            {
+                Type ColumnType = pi.PropertyType;
+                if ((ColumnType.IsGenericType))
+                {
+                    ColumnType = ColumnType.GetGenericArguments()[0];
+                }
+                dt.Columns.Add(pi.Name, ColumnType);
+            }
+
+            //Populate the data table
+            foreach (AccountModel item in AcList)
+            {
+                DataRow dr = dt.NewRow();
+                dr.BeginEdit();
+                foreach (PropertyInfo pi in pia)
+                {
+                    if (pi.GetValue(item, null) != null)
+                    {
+                        dr[pi.Name] = pi.GetValue(item, null);
+                    }
+                }
+                dr.EndEdit();
+                dt.Rows.Add(dr);
+            }
+            Grid.DataSource = dt;
+        }
+
         public void SetConnection()
         {
-            sql_con = new SQLiteConnection(@"Data Source=C:\Users\motivar\Desktop\Database\Database_Prototype.db;Version=3;");
+            sql_con = new SQLiteConnection(@"Data Source=C:\Users\motivar\OneDrive\MOTIVAR\Database_Prototype.db;Version=3;");
         }
+
+        //public DataTable ToDataTable<AccountModel>(this IEnumerable<AccountModel> collection)
+        //{
+        //    DataTable dt = new DataTable("DataTable");
+        //    Type t = typeof(AccountModel);
+        //    PropertyInfo[] pia = t.GetProperties();
+
+        //    //Inspect the properties and create the columns in the DataTable
+        //    foreach (PropertyInfo pi in pia)
+        //    {
+        //        Type ColumnType = pi.PropertyType;
+        //        if ((ColumnType.IsGenericType))
+        //        {
+        //            ColumnType = ColumnType.GetGenericArguments()[0];
+        //        }
+        //        dt.Columns.Add(pi.Name, ColumnType);
+        //    }
+
+        //    //Populate the data table
+        //    foreach (T item in collection)
+        //    {
+        //        DataRow dr = dt.NewRow();
+        //        dr.BeginEdit();
+        //        foreach (PropertyInfo pi in pia)
+        //        {
+        //            if (pi.GetValue(item, null) != null)
+        //            {
+        //                dr[pi.Name] = pi.GetValue(item, null);
+        //            }
+        //        }
+        //        dr.EndEdit();
+        //        dt.Rows.Add(dr);
+        //    }
+        //    return dt;
+        //}
 
         public dbForm()
         {
@@ -50,8 +148,11 @@ namespace SqliteDB
             dbconnect.dbConnection();
             dbconnect.dbCreateTable();*/
 
-            dbconn();
+            //dbconn();
+            dbReadToList();
+            ListToDataSet();
 
+            //ConnectGoogleDrive.Connect();
 
         }
     }
