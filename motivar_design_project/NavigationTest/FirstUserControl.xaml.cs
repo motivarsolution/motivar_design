c5aced1cfef2539097edd371f5f71958a1f52838
TΛП WUTTISAKDANON <hadson_sm@hotmail.com>
2017-10-21 12:03:04 +0700

    Add NavigationTest

---
+++ motivar_design_project/NavigationTest/FirstUserControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NavigationTest
+{
+    /// <summary>
+    /// Interaction logic for FirstUserControl.xaml
+    /// </summary>
+    public partial class FirstUserControl : UserControl
+    {
+        public FirstUserControl()
+        {
+            InitializeComponent();
+
+            UCGrid.Children.Add(new TextBlock{Text = "TEST Text User Control" ,
+                                              HorizontalAlignment = HorizontalAlignment.Center,
+                                              VerticalAlignment = VerticalAlignment.Center,
+                                              FontSize = 28
+                                });
+        }
+    }
+}
